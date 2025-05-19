fc74149bea3fefba21f1e83b6fa27d940f64fb58
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-19 19:34:39 +0800

    Update ollama_tools.R

--- R/ollama/ollama_tools.R
+++ R/ollama/ollama_tools.R
@@ -10,25 +10,10 @@ const build_ollama_tools = function(deepseek = NULL) {
 
         nchar(ollama) > 0; 
     });
-        
-    for(let func in ollama_tools) {
-        # [@ollama "tool_name"]
-        let attrs = .Internal::attributes(func);
-        let tool_name = attrs$ollama;
-        let roxygon = as.list(.Internal::docs(func));
-        let desc_str = which([roxygon$description, roxygon$details], s -> nchar(s) > 0);
-        let args = lapply(roxygon$parameters, t -> t$text, names = t -> t$name);
-        let requires = which(roxygon$declares$parameters, p -> is.null(p$text)) |> sapply(p -> p$name);
 
-        cat(`found ollama tool: ${tool_name}\n`);
-
-        ollama::add_tool(deepseek, 
-            name = tool_name, 
-            desc = paste(desc_str, sepc = " "),
-            requires = requires,
-            args = args) = func;
-    }
-
+    # should put this function tool at very first
+    # so that other function tools that with the exact same name ``sys_info`` 
+    # which from other loaded packages could overrides this function 
     ollama::add_tool(deepseek, 
         name = "sys_info", 
         desc = "get information about yourself when the user want you to introduce your self. this function requires no parameters.",
@@ -44,4 +29,24 @@ const build_ollama_tools = function(deepseek = NULL) {
             "license": sys_info$license
         };
     };
+
+    # scan R# runtime and load function call tools 
+    # into the ollama client
+    for(let func in ollama_tools) {
+        # [@ollama "tool_name"]
+        let attrs = .Internal::attributes(func);
+        let tool_name = attrs$ollama;
+        let roxygon = as.list(.Internal::docs(func));
+        let desc_str = which([roxygon$description, roxygon$details], s -> nchar(s) > 0);
+        let args = lapply(roxygon$parameters, t -> t$text, names = t -> t$name);
+        let requires = which(roxygon$declares$parameters, p -> is.null(p$text)) |> sapply(p -> p$name);
+
+        cat(`found ollama tool: ${tool_name}\n`);
+
+        ollama::add_tool(deepseek, 
+            name = tool_name, 
+            desc = paste(desc_str, sepc = " "),
+            requires = requires,
+            args = args) <- func;
+    }
 }
