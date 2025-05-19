6ea73f11cff7b5ec684fd695843dddfea7b3dceb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-19 14:25:20 +0800

    create demo ollama utils

---
+++ R/ollama/utils.R
@@ -0,0 +1,8 @@
+#' read the given file as plain text file, this function returns the plain text data of the input file
+#' 
+#' @param file the file path of the target text file for read by this function
+#' 
+[@ollama "read_text"]
+const read_text = function(file) {
+    .Internal::readText(file);
+}
