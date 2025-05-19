f7813a6032880e0658cc5c6f34c1dcd435413994
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-19 17:42:24 +0800

    Update app.R

--- run/app.R
+++ run/app.R
@@ -2,13 +2,11 @@ require(Athena);
 
 include(relative_work("../etc/app.json"));
 
-# title: R# web http server
-# author: xieguigang
-# description: a commandline R# script for running a http web server.
-const httpPort as integer  = getOption("listen");
-const webContext as string = relative_work("../web/");
-const deepseek = Athena::init_ollama();
-
-run_http(deepseek, httpPort, webContext);
+# run app
+run_http(
+  deepseek = Athena::init_ollama(), 
+  httpPort = getOption("listen"), 
+  webContext = relative_work("../web/")
+);
 
 
