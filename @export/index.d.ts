0d3c8d1916e8769176aee1a5ca79ea0b6de4cc80
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-19 16:32:54 +0800

    run package msbuild

---
+++ @export/index.d.ts
@@ -0,0 +1,21 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=Athena
+
+declare namespace Athena {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+   /**
+     * @param deepseek default value Is ``null``.
+   */
+   function build_ollama_tools(deepseek?: any): object;
+   /**
+   */
+   function init_ollama(): object;
+   /**
+   */
+   function read_text(file: any): object;
+}
