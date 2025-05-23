dc358731855dcfa6d2de8bd0d2c953fe4c287be8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-24 01:53:11 +0800

    try to fix bug of ai show url

--- src/athena_ui/ai.ts
+++ src/athena_ui/ai.ts
@@ -26,10 +26,14 @@ module ai_chat {
     export function format_html(out: output | string): string {
         let markedjs: markdown = (<any>window).marked;
 
+        // the AI has a bug about file url
+        // always has a localhost prefix, example as http://localhost:8000
+        // removes this prefix so that remote user can access the
+        // server file via the relative url
         if (typeof out == "string") {
-            return out;
+            return out ? out.replace(/http[:]\/\/localhost[:]\d+/g, "") : "";
         } else {
-            return markedjs.parse(out.output);
+            return markedjs.parse(out.output.replace(/http[:]\/\/localhost[:]\d+/g, ""));
         }
     }
 }
